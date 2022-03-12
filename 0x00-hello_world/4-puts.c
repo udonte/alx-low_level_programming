6892f472fd485baa3d40323757efcddf1151710b
George Udonte <georgeudonte@gmail.com>
2022-03-12 08:29:24 -0800

    c file that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** main - prints " programming is like building a multilingual puzzle "
+ * Return: 0 if exited propely, non zero otherwise"
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
