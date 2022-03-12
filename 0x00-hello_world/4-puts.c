476c2278d7326806a9113c9ebfb1313b081ee2c3
George Udonte <georgeudonte@gmail.com>
2022-03-12 08:33:10 -0800

    c file that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-/** main - prints " programming is like building a multilingual puzzle "
- * Return: 0 if exited propely, non zero otherwise"
+/** 
+ * main - prints  "programming is like building a multilingual puzzle"
+ * Return: 0 if exited propely, non zero otherwise
  */
 int main(void)
 {
