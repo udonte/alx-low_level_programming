b4717b20c33f3949433b855f7fadda89ed62844c
George Udonte <96987540+udonte@users.noreply.github.com>
2022-03-14 10:17:18 +0100

    Updated positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,12 +4,13 @@
 
 /**
 * main - assigns a random number to int n everytime
-* it executes, and prints it
+* 
 * Return: Always 0 (Success)
 */
 int main(void)
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
